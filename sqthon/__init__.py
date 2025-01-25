528902100a8078024562c9cdad9741687241f1a4
HIMRAJ <iamhimraj002077@gmail.com>
2025-01-25 12:29:22 +0530

    fixed init.py

--- sqthon/__init__.py
+++ sqthon/__init__.py
@@ -1,7 +1,4 @@
 from .main import Sqthon
-from .connection import DatabaseConnector
 from .data_visualizer import DataVisualizer
-from .llm import SqthonAI
 
-
-__all__ = ["Sqthon", "DatabaseConnector", "DataVisualizer", "SqthonAI"]
+__all__ = ["Sqthon", "DataVisualizer"]
